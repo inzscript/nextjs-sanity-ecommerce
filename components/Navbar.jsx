261c54de622306af2ae8aa8002ecd7cec952202d
Reggie Stevens <inzscript@reggies-air.lan>
2022-05-25 18:31:32 -0700

    Add shopping cart feature

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
 
+import { Cart } from './'
+import { useStateContext } from '../context/StateContext'
+
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Headspace</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick="">
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">2</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
 
-      {/* {showCart && <Cart />} */}
+      { showCart && <Cart /> }
     </div>
   )
 }
